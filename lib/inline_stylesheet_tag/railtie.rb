5a5fbfc302a27a09bfab1ee57c53e9c5e2c34734
Tejas Dinkar <tejas@gja.in>
2017-05-23 09:24:10 -0700

    Fixing an include thing

--- lib/inline_stylesheet_tag/railtie.rb
+++ lib/inline_stylesheet_tag/railtie.rb
@@ -1,5 +1,5 @@
 class InlineStylesheetTag::Railtie < Rails::Railtie
   initializer "inline_stylesheet_tag.view_helpers" do
-    ActionView::Base.send :include, ViewHelpers
+    ActionView::Base.send :include, InlineStylesheetTag::ViewHelpers
   end
 end
